2a7debc37bd97281e82d0297528051a470f8649c
Maksim6767 <nikmaksim20@gmail.com>
2023-01-22 22:07:47 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+import { Layout } from './Layout/Layout';
+import { Appbar } from './AppBar/Appbar';
+import { TaskForm } from 'components/TaskForm/TaskForm';
+import { TaskList } from 'components/TaskList/TaskList';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <Appbar />
+      <TaskForm />
+      <TaskList />
+    </Layout>
   );
 };
