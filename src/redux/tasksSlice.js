7071ef39a876263a81525d94e73308102ed820eb
Maksim6767 <nikmaksim20@gmail.com>
2023-01-27 22:32:47 +0200

    toolkit slice

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -9,39 +9,37 @@ const tasksInitialState = [
 ];
 
 const tasksSlice = createSlice({
-    name: "addtasks",
-    initialState: tasksInitialState,
-    reducers: {
-        addTask: {
-            reducer(state, action) {
-                state.push(action.payload)
-            },
-            prepare(text) {
-                return {
-                    payload: {
-                        text,
-                        id: nanoid(),
-                        completed: false,
-                    },
-                };
-            },
-        },
-        deleteTask(state, action) {
-            const index = state.findIndex(task => task.id === action.payload);
-            state.splice(index, 1);
-        },
-        toggleCompleted(state, action) {
-            for (const task of state) {
-                if (task.id === action.payload) {
-                    task.completed = !task.completed;
-                    
-                }
-            }
-        },
+  name: "tasks",
+  initialState: tasksInitialState,
+  reducers: {
+    addTask: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(text) {
+        return {
+          payload: {
+            text,
+            id: nanoid(),
+            completed: false,
+          },
+        };
+      },
     },
+    deleteTask(state, action) {
+      const index = state.findIndex(task => task.id === action.payload);
+      state.splice(index, 1);
+    },
+    toggleCompleted(state, action) {
+      for (const task of state) {
+        if (task.id === action.payload) {
+          task.completed = !task.completed;
+          break;
+        }
+      }
+    },
+  },
 });
 
-// Экспортируем генераторы экшенов
 export const { addTask, deleteTask, toggleCompleted } = tasksSlice.actions;
-// Экспортируем редюсер
 export const tasksReducer = tasksSlice.reducer;
